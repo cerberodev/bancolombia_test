1161da1d94fee2224051bc8800aabcd0483b96ff
Your Name <you@example.com>
2020-10-20 12:58:28 -0500

    Back action

--- lib/bancolombia_splash_page.dart
+++ lib/bancolombia_splash_page.dart
@@ -6,6 +6,7 @@ class BancolombiaSplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: BancolombiaAppBar(),
     );
   }
--- lib/widgets/bancolombia_app_bar.dart
+++ lib/widgets/bancolombia_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_project/bancolombia_splash_page.dart';
 
 class BancolombiaAppBar extends StatelessWidget with PreferredSizeWidget {
   final String appBarTitle;
@@ -7,13 +8,32 @@ class BancolombiaAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: true,
+      leading: Row(children: [
+        InkWell(
+            onTap: () {
+              if (Navigator.of(context).canPop()) {
+                Navigator.of(context).pop();
+              }
+            },
+            child: Icon(Icons.arrow_back)),
+        Icon(Icons.ac_unit),
+      ]),
       centerTitle: true,
       title: appBarTitle != null ? _createTitle() : ImageAppBar(),
       actions: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Icon(
-            Icons.add,
+        InkWell(
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return BancolombiaSplashPage();
+            }));
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.add,
+            ),
           ),
         )
       ],
