faba30ec99d96072062bbd0762dfc6431127ab4d
Your Name <you@example.com>
2020-10-20 14:29:30 -0500

    ListView

--- lib/bancolombia_splash_page.dart
+++ lib/bancolombia_splash_page.dart
@@ -5,9 +5,85 @@ import 'widgets/bancolombia_app_bar.dart';
 class BancolombiaSplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    height = height - 16;
+    height = height - kToolbarHeight;
+    height = height - MediaQuery.of(context).padding.top;
+    height = height / 2;
     return Scaffold(
       drawer: Drawer(),
-      appBar: BancolombiaAppBar(),
+      appBar: BancolombiaAppBar(
+        appBarTitle: "Home",
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Container(
+              height: height,
+              child: ListView.builder(
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int pos) {
+                  return Text(names[pos]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+List names = [
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel',
+  'Nombre',
+  'Apellido',
+  'Juan',
+  'Luis',
+  'Manuel'
+];
